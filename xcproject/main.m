506fde70310675e8a7d0ff87fadb16d304a18464
Brian Garrett <popavo@gmail.com>
2013-12-17 13:27:32 -0600

    Initial Commit

---
+++ xcproject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xcproject
+//
+//  Created by Brian K Garrett on 12/17/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
