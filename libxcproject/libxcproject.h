a054ffb17144b09cbfa2c07cf7e3aea73f843148
Brian Garrett <popavo@gmail.com>
2014-02-09 17:32:56 -0600

    created an external ruby library target

---
+++ libxcproject/libxcproject.h
@@ -0,0 +1,14 @@
+//
+//  libxcproject.h
+//  libxcproject
+//
+//  Created by Brian K Garrett on 1/21/14.
+//
+//
+
+#ifndef LIBXCPROJECT_H
+#define LIBXCPROJECT_H
+
+
+
+#endif
---
+++ libxcproject/libxcproject.mm
@@ -0,0 +1,13 @@
+//
+//  libxcproject.m
+//  libxcproject
+//
+//  Created by Brian K Garrett on 1/21/14.
+//
+//
+
+#import "libxcproject.h"
+#import <unordered_map>
+#import <string>
+#import <Ruby/ruby.h>
+
